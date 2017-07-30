21e61699e6c246bc19ec36e7225c06ee01711365
Victor Borja <vborja@apache.org>
2017-07-30 07:48:21 -0500

    table

--- generate-table.sh
+++ generate-table.sh
@@ -9,7 +9,7 @@ gh="https://github.com/$repo"
 echo '| Status | Dockerfile |'
 echo '|--------|------------|'
 for i in $(curl  https://api.github.com/repos/$gh_repo/branches 2>/dev/null | jq -r .[].name); do
-  echo -n "| [![Travis branch](https://img.shields.io/travis/$gh_repo/$i.svg)]($gh/tree/$i)"
+  echo -n "| [![Travis branch](https://img.shields.io/travis/$gh_repo/$i.svg)](https://travis-ci.org/$gh_repo/branches#$i)"
   echo -n "| [\`FROM $repo:$i\`]($gh/blob/$i/Dockerfile) "
   echo '|'
 done
